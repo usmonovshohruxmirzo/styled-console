996af029a9517d7218ce9702acae87ef0d4b48c7
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2024-12-11 19:25:54 +0500

    feat: themes

--- index.js
+++ index.js
@@ -2,12 +2,43 @@ function camelToKebabCase(camel) {
   return camel.replace(/([A-Z])/g, "-$1").toLowerCase();
 }
 
-function styledConsole(text, styles = {}) {
+function themes(type) {
+  const colors = {
+    primary: 210,
+    secondary: 220,
+    success: 120,
+    info: 200,
+    warning: 40,
+    help: 280,
+    danger: 0,
+  };
+
+  switch (type) {
+    case "primary":
+      return `hsl(${colors.primary}, 85%, calc(50 * 1%))`;
+    case "danger":
+      return `hsl(${colors.danger}, 85%, calc(50 * 1%))`;
+    case "help":
+      return `hsl(${colors.help}, 85%, calc(50 * 1%))`;
+    case "info":
+      return `hsl(${colors.info}, 85%, calc(50 * 1%))`;
+    case "secondary":
+      return `hsl(${colors.secondary}, 85%, calc(50 * 1%))`;
+    case "success":
+      return `hsl(${colors.success}, 85%, calc(50 * 1%))`;
+    case "warning":
+      return `hsl(${colors.warning}, 85%, calc(50 * 1%))`;
+    default:
+      return type;
+  }
+}
+
+function styledConsole(text, styles = {}, type = "primary") {
   const defaultStyles = {
-    color: "blue",
+    color: "#fff",
     fontSize: "16px",
-    background: "#fff",
-    padding: "50px",
+    background: themes(type),
+    padding: "10px",
   };
 
   const combinedStyles = { ...defaultStyles, ...styles };
