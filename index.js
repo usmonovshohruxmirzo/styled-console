71cb39e3b4ee7927096332d66f0ad2032b662166
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2024-12-11 09:57:05 +0500

    feat: error handlers

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ function camelToKebabCase(camel) {
   return camel.replace(/([A-Z])/g, "-$1").toLowerCase();
 }
 
-function styledConsole(text, styles) {
-  const stylesObj = Object.entries(styles)
-    .map(([key, value]) => `${camelToKebabCase(key)}: ${value}`)
-    .join("; ");
+function styledConsole(text, styles = {}) {
+  try {
+    const stylesObj = Object.entries(styles)
+      .map(([key, value]) => `${camelToKebabCase(key)}: ${value}`)
+      .join("; ");
 
-  console.log(`%c${text}`, stylesObj);
+    console.log(`%c${text}`, stylesObj);
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message);
+  }
 }
 
 export default styledConsole;
